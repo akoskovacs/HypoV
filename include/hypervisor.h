e0525f04705a64299b4a481500565f5fb7e9f5bb
Ákos Kovács <akoskovacs0@gmail.com>
2017-10-29 11:21:42 +0100

    Add headers

--- include/hypervisor.h
+++ include/hypervisor.h
@@ -1,3 +1,10 @@
+/*
+ * +------------------------------------------------------------+
+ * | Copyright (C) Ákos Kovács - 2017                           |
+ * |                                                            |
+ * | Hypervisor system functions                                |
+ * +------------------------------------------------------------+
+*/
 #ifndef HYPERVISOR_H
 #define HYPERVISOR_H
 
--- sys/init.c
+++ sys/init.c
@@ -1,3 +1,10 @@
+/*
+ * +------------------------------------------------------------+
+ * | Copyright (C) Ákos Kovács - 2017                           |
+ * |                                                            |
+ * | Hypervisor 64 bit initialization code                      |
+ * +------------------------------------------------------------+
+*/
 #include <hypervisor.h>
 
 void hv_entry(void)
