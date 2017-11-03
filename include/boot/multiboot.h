f5d51cb6a69a377227dff68b0e09cf7a7c6fba50
Ákos Kovács <akoskovacs0@gmail.com>
2017-11-03 17:59:18 +0100

    Add multiboot header

---
+++ include/boot/multiboot.h
@@ -0,0 +1,80 @@
+/*
+ * +------------------------------------------------------------+
+ * | Copyright (C) Ákos Kovács - 2017                           |
+ * |                                                            |
+ * | Support structs for SYSLINUX's multiboot feature           |
+ * +------------------------------------------------------------+
+*/
+#ifndef MULTIBOOT_H
+#define MULTIBOOT_H
+
+#ifdef __HVISOR__
+# include <types.h>
+#else
+# include <inttypes.h>
+#endif 
+
+#include <basic.h>
+
+enum MULTIBOOT_FLAGS {
+    MB_INFO_MEMORY        = 0x0001,
+    MB_INFO_BOOTDEV       = 0x0002,
+    MB_INFO_CMDLINE       = 0x0004,
+    MB_INFO_MODS          = 0x0008,
+    MB_INFO_ELF_SECT      = 0x0020,
+    MB_INFO_MEM_MAP       = 0x0040,
+    MB_INFO_DRIVE_INFO    = 0x0080,
+    MB_INFO_CONFIG_TABLE  = 0x0100,
+    MB_INFO_BOOT_LOADER   = 0x0200,
+    MB_INFO_APM_TABLE     = 0x0400,
+    MB_INFO_VIDEO         = 0x0800
+};
+
+struct __packed MultiBootElfSection {
+    uint32_t num;
+    uint32_t size;
+    uint32_t addr;
+    uint32_t shndx;
+};
+
+struct __packed MultiBootMmapEntry {
+    uint32_t size;
+    uint64_t addr;
+    uint64_t length;
+#define MB_MEMORY_AVAILABLE 1
+#define MB_MEMORY_RESERVED 2
+    uint32_t type;
+};
+
+struct __packed MultiBootModule {
+    uint32_t mod_start;
+    uint32_t mod_end;
+    uint32_t cmdline;
+    uint32_t pad;
+};
+
+struct __packed MultiBootInfo {
+    uint32_t flags;
+    uint32_t mem_lower;
+    uint32_t mem_upper;
+    uint32_t boot_device;
+    uint32_t cmdline;
+    uint32_t mods_count;
+    uint32_t mods_addr;
+    struct   multiboot_elf_section elf_sec;
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+    uint32_t drives_length;
+    uint32_t drivers_addr;
+    uint32_t config_table;
+    uint32_t boot_loader_name;
+    uint32_t apm_table;
+    uint32_t vbe_control_info;
+    uint16_t vbe_mode_info;
+    uint16_t vbe_mode;
+    uint16_t vbe_interface_seg;
+    uint16_t vbe_interface_off;
+    uint16_t vbe_interface_len;
+};
+
+#endif // MULTIBOOT_H
