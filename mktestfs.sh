342133f714762d9cac7f4392cd44c4d585f3dba1
Ákos Kovács <akoskovacs0@gmail.com>
2017-11-05 17:13:49 +0100

    Add test filesystem generator script for SYSLINUX, fix #21

---
+++ mktestfs.sh
@@ -0,0 +1,30 @@
+# Make a FAT32 test file system and install
+# the hypervisor with SYSLINUX on it
+
+#!/bin/bash
+
+TESTFS_NAME=testfs.img
+TESTFS_LABEL=PENDRIVE
+TESTFS_SIZE=65M
+TMP=/tmp/fs
+
+echo "Making FAT32 filesystem '$TESTFS_NAME'..."
+dd if=/dev/zero of=$TESTFS_NAME bs=$TESTFS_SIZE count=1
+mkfs.fat -n "$TESTFS_LABEL" -F32 $TESTFS_NAME
+echo -n "Installing SYSLINUX bootloader..."
+syslinux -i $TESTFS_NAME && echo "[OK]"
+echo -n "Mounting the new filesystem at '$TMP'..."
+mkdir $TMP
+sudo mount $TESTFS_NAME $TMP && echo "[OK]"
+echo -n "Installing SYSLINUX files..."
+sudo cp etc/syslinux_pendrive/* $TMP && echo "[OK]"
+echo -n "Installing HypoV..."
+sudo cp hypov.bin $TMP && echo "[OK]"
+echo -n "Unmounting the filesystem..."
+sudo umount $TMP && echo "[DONE]"
+rm -rf $TMP
+
+echo
+echo "You can test the setup, issuing: "
+echo "$ qemu-system-i386 -hda $TESTFS_NAME"
+
