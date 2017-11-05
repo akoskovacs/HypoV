d63e60a08bd12f2215ef8051fd7ae7f8671ed9c0
Ákos Kovács <akoskovacs0@gmail.com>
2017-11-05 19:42:56 +0100

    Test console

--- sys/init.c
+++ sys/init.c
@@ -2,16 +2,31 @@
  * +------------------------------------------------------------+
  * | Copyright (C) Ákos Kovács - 2017                           |
  * |                                                            |
- * | Hypervisor 64 bit initialization code                      |
+ * | Hypervisor initialization code                             |
  * +------------------------------------------------------------+
 */
-#include <boot/multiboot.h>
 #include <basic.h>
+#include <boot/multiboot.h>
+#include <types.h>
 #include <hypervisor.h>
+
+volatile uint16_t *videomem = (uint16_t *)0xb8000;
+#define CONSOLE_WIDTH  80
+#define CONSOLE_HEIGHT 25
 
 void __noreturn 
 hv_entry(struct MultiBootInfo *mbi, unsigned int magic)
 {
+    int i;
+    const char hw[] = "HypoV - Copyright (C) Akos Kovacs";
+    for (i = 0; i < CONSOLE_HEIGHT*CONSOLE_WIDTH; i++) {
+        videomem[i] = ((0x47) << 8);
+    }
+
+    for (i = 0; i < sizeof(hw); i++) {
+        videomem[i] = (0x47 << 8) | hw[i];
+    }
+
     while (1) 
         ;
 }
