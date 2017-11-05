b8d921b03908ad4143d91c7b825040ab3b1d492d
Ákos Kovács <akoskovacs0@gmail.com>
2017-11-05 18:48:57 +0100

    Multiboot header added

--- boot/entry.asm
+++ boot/entry.asm
@@ -7,8 +7,28 @@
 global hv_multiboot_entry
 extern hv_entry
 
+; Multiboot definitions
+%define MB_ALIGN        0x001
+%define MB_MEMINFO      0x002
+%define MB_FLAGS        MB_ALIGN | MB_MEMINFO
+
+%define MB_MAGIC        0x1BAD002
+%define MB_CHECKSUM     -(MB_MAGIC + MB_FLAGS)
+
+; Multiboot header start
 section .multiboot_header
+dq MB_MAGIC
+dq MB_FLAGS
+dq MB_CHECKSUM
+dq 0 ; Header address
+dq 0 ; Loader address
+dq 0 ; Loader address end
+dq 0 ; BSS address end
+dq 0 ; Entry-point address
+dq 0 ; Mode type
 
 bits 32
+align 4
 hv_multiboot_entry:
     call hv_entry
+    jmp $
