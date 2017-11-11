835693a8ea6f298d504f679b9ef988f18409ce71
Ákos Kovács <akoskovacs0@gmail.com>
2017-11-11 19:16:17 +0100

    Streamline hv_entry(), remove old stuff

--- sys/init.c
+++ sys/init.c
@@ -11,20 +11,16 @@
 #include <hypervisor.h>
 #include <pc_console.h>
 
-volatile uint16_t *videomem = (uint16_t *)0xb8000;
-#define CONSOLE_WIDTH  80
-#define CONSOLE_HEIGHT 25
-
 struct ConsoleDisplay main_display;
+struct CharacterDisplay *disp = (struct CharacterDisplay *)&main_display;
 
 void __noreturn hv_entry(struct MultiBootInfo *mbi, unsigned int magic)
 {
-    int i;
     const char hw[] = "HypoV Hypervisor - Copyright (C) Akos Kovacs";
 
     hv_console_display_init(&main_display);
-    hv_disp_clear((struct CharacterDisplay *)&main_display);
-    hv_set_stdout(&main_display);
+    hv_disp_clear(disp);
+    hv_set_stdout(disp);
 
     puts(hw);
 
