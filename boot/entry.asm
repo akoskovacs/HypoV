e04c2470e1a70e384310d0d1df941b2cc822ca6c
Ákos Kovács <akoskovacs0@gmail.com>
2017-11-11 19:09:17 +0100

    Refactoring linker script and the multiboot header

--- boot/entry.asm
+++ boot/entry.asm
@@ -5,6 +5,7 @@
 ; +------------------------------------------------------------+
 
 global hv_multiboot_entry
+global hv_multiboot_header
 extern hv_entry
 
 %define HV_STACK_SIZE 4096 ; 4Kb stack
@@ -18,20 +19,22 @@ extern hv_entry
 %define MB_CHECKSUM     -(MB_MAGIC + MB_FLAGS)
 
 ; Multiboot header start
-section .multiboot_header
-dq MB_MAGIC
-dq MB_FLAGS
-dq MB_CHECKSUM
-dq 0 ; Header address
-dq 0 ; Loader address
-dq 0 ; Loader address end
-dq 0 ; BSS address end
-dq 0 ; Entry-point address
-dq 0 ; Mode type
+align 4
+section .multiboot.header
+multiboot_header_start:
+    dd MB_MAGIC
+    dd MB_FLAGS
+    dd MB_CHECKSUM
+    dd 0 ; Header address
+    dd 0 ; Loader address
+    dd 0 ; Loader address end
+    dd 0 ; BSS address end
+    dd 0 ; Entry-point address
+    dd 0 ; Mode type
 
+section .multiboot.text
 bits 32
 align 4
-
 ; The entry point for the multiboot bootloader
 hv_multiboot_entry:
 ; Bochs breakpoint
@@ -45,7 +48,8 @@ hv_multiboot_entry:
 ; Call the 32 bit C entry function in sys/init.c
 ; hv_entry(struct MultiBootInfo *, int magic)
     call hv_entry
-    jmp $
+.hang:
+    jmp .hang
 
 section .bss
 hv_stack: resb HV_STACK_SIZE
