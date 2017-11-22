c98fbc58d3a74e66861188d5aef1072364eb2a1b
Ákos Kovács <akoskovacs0@gmail.com>
2017-11-22 02:28:21 +0100

    Pass bootloader info the the debug screen, refactor the bottom menu

--- include/debug_console.h
+++ include/debug_console.h
@@ -10,6 +10,7 @@
 
 #include <drivers/video/pc_console.h>
 #include <types.h>
+#include <boot/multiboot.h>
 
 struct DebugScreen;
 
@@ -31,11 +32,12 @@ struct DebugScreen {
     #endif
 };
 
-int dc_start(struct ConsoleDisplay *);
+int dc_start(struct ConsoleDisplay *, struct MultiBootInfo *mbi);
 int dc_init(struct ConsoleDisplay *);
 int dc_show_screen(struct DebugScreen *);
 int dc_top_menu_draw(struct DebugScreen *);
 int dc_bottom_menu_draw(struct DebugScreen *);
 int dc_keyboard_handler(char scancode);
+int dc_bottom_show_message(struct DebugScreen *scr, const char *message);
 
 #endif // DEBUG_CONSOLE_H
