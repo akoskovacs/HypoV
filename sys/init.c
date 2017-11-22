a75469213f7b00bf02a2efd3aff862115d9d9a3b
Ákos Kovács <akoskovacs0@gmail.com>
2017-11-22 02:34:37 +0100

    Remove old code from hv_entry(), pass the multiboot address
            * Fix __noreturn warning

--- sys/init.c
+++ sys/init.c
@@ -16,21 +16,16 @@
 struct ConsoleDisplay main_display;
 struct CharacterDisplay *disp = (struct CharacterDisplay *)&main_display;
 
+/* If the magic number is wrong, mbi will be NULL */
 void __noreturn hv_entry(struct MultiBootInfo *mbi, unsigned int magic)
 {
-#if 0
-    const char hw[] = "HypoV Hypervisor - Copyright (C) Akos Kovacs";
-
-    hv_console_display_init(&main_display);
-    hv_disp_clear(disp);
-    hv_set_stdout(disp);
-
-    puts(hw);
-#endif 
     hv_console_cursor_disable();
     hv_console_display_init(&main_display);
     hv_set_stdout(disp);
-    dc_start(&main_display);
+    dc_start(&main_display, mbi);
 
     keyboard_loop(dc_keyboard_handler);
+
+    while (1)
+        ;
 }
