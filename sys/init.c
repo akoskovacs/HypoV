14c1b0e8e4baf9e6100539224236786046281bf7
Ákos Kovács <akoskovacs0@gmail.com>
2017-11-25 05:57:18 +0100

    Call keyboard_init() and hv_mm_init_mapping() from hv_init()
            * No need for global variables

--- sys/init.c
+++ sys/init.c
@@ -12,17 +12,21 @@
 #include <drivers/video/pc_console.h>
 #include <drivers/input/pc_keyboard.h>
 #include <debug_console.h>
-
-struct ConsoleDisplay main_display;
-struct CharacterDisplay *disp = (struct CharacterDisplay *)&main_display;
+#include <memory.h>
 
 /* If the magic number is wrong, mbi will be NULL */
 void __noreturn hv_entry(struct MultiBootInfo *mbi, unsigned int magic)
 {
-    hv_console_cursor_disable();
+    struct ConsoleDisplay main_display;
+    struct CharacterDisplay *disp = (struct CharacterDisplay *)&main_display;
+    struct PhysicalMMapping *system_mmap = NULL;
+
+    keyboard_init();
     hv_console_display_init(&main_display);
     hv_set_stdout(disp);
-    dc_start(&main_display, mbi);
+    system_mmap = hv_mm_init_mapping(mbi);
+    hv_console_cursor_disable();
+    dc_start(&main_display, mbi, system_mmap);
 
     keyboard_loop(dc_keyboard_handler);
 
