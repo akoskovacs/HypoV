a136b8abf695e75ab424669f61c69fa4c68fb9ff
Ákos Kovács <akoskovacs0@gmail.com>
2017-12-29 08:27:24 +0100

    Add documentation, cleanup code

--- hvisor/hvisor.c
+++ hvisor/hvisor.c
@@ -1,29 +1,51 @@
+/*
+ * +------------------------------------------------------------+
+ * | Copyright (C) Ákos Kovács - 2017                           |
+ * |                                                            |
+ * | The actual 64-bit hypervisor payload, embedded in the main |
+ * | ELF32 image.                                               |
+ * +------------------------------------------------------------+
+*/
+
+/* 
+ * This code is contained in a proper ELF64 binary image. It is built
+ * completily statically, without dependencies and shared libraries (including libc).
+ * But, by mistake it could still be executed in a standard Linux/BSD/etc environment.
+ * If it is not loaded by the HypoV loader, the code will print an error message through
+ * standard Linux system call interface (we don't have libc, see above).
+*/
+
+#define SCALL_LINUX_WRITE 1
+#define SCALL_LINUX_EXIT 60
+
 static char hv_exe_warning[] = "HypoV cannot be used from operating systems.\n";
 
 long scall_linux_write(long fd, const void *data, unsigned long size)
 {
     long retcode = 0;
-    __asm__ __volatile__(
-         /* "movq $1, %rax\n\t"
-            "movq %0, %%rdi\n\t"
-            "movq %1, %%rsi\n\t"
-            "movq %2, %%rdx\n\t" */
-            "syscall"
+    /* 
+     * Using Linux x86_64 system call interface
+     *   %rax = write() system call id
+     *   %rdi = fd - file descriptor
+     *   %rsi = data - data to write
+     *   %rdx = size -  size of the data
+    */
+    __asm__ __volatile__( "syscall"
             : "=a"(retcode)
-            : "a"(1), "D"(fd), "S"(data), "d"(size));
-            // : "rdi", "rsi", "rdx");
+            : "a"(SCALL_LINUX_WRITE), "D"(fd), "S"(data), "d"(size));
     return retcode;
 
 }
 
-void scall_linux_exit(long n)
+void scall_linux_exit(long ecode)
 {
-    __asm__ __volatile__(
-         /* "movq $60, %%rax\n\t"
-            "movq %0, %%rdi\n\t" */
-            "syscall"
+    /* 
+     * %rax = exit() system call id
+     * %rdi = ecode - exit code 
+    */
+    __asm__ __volatile__( "syscall"
             : // No output
-            : "a"(60), "D"(n));
+            : "a"(SCALL_LINUX_EXIT), "D"(ecode));
 }
 
 void hv_entry_64(void *arg)
@@ -35,6 +57,7 @@ void hv_entry_64(void *arg)
         return;
     } 
 
+    // TODO
     while (1) {
         ;
     }
