5175045dee433b7ac335cf04d9e17b85f571098c
Ákos Kovács <akoskovacs0@gmail.com>
2017-12-31 09:46:44 +0100

    Remove unncessary test code

--- sys/init.c
+++ sys/init.c
@@ -21,20 +21,17 @@ static struct SystemInfo system_info;
 void __noreturn hv_entry(struct MultiBootInfo *mbi)
 {
     struct ConsoleDisplay main_display;
-    int error = 0;
 
     system_info.s_boot_info = mbi;
-    system_info.s_display   = (struct CharacterDispay *)&main_display;
+    system_info.s_display   = (struct CharacterDisplay *)&main_display;
 
     keyboard_init();
     hv_console_display_init(&main_display);
     hv_set_stdout(system_info.s_display);
-    system_info.s_phy_maps = hv_mm_init_mapping(mbi);
+    system_info.s_phy_maps = mm_init_mapping(mbi);
     system_info.s_cpu_info = cpu_get_info();
     hv_console_cursor_disable();
     dc_start(&system_info);
-
-    system_info.s_core_image = elf64_load((void*)hv_entry, 0x00FFFFFFFF, &error);
 
     keyboard_loop(dc_keyboard_handler);
 
