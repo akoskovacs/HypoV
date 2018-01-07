4191512986524782a0332fcb517f82cfc5813656
Ákos Kovács <akoskovacs0@gmail.com>
2018-01-07 23:25:39 +0100

    Improve ELF64 loader

--- include/loader.h
+++ include/loader.h
@@ -11,18 +11,26 @@
 
 #include <elf.h>
 
+struct MemoryMap;
+
 struct Elf64_Image
 {
-    struct Elf64_Hdr   *i_header;
-    struct Elf64_Shdr **i_sections;      /* NULL terminated */
-    size_t              i_nr_sections;
-    char              **i_strtab;        /* NULL terminated */
-    size_t              i_nr_strtab;
+    void               *i_start;
+    void               *i_end;
 
-    void (*i_entry)(void);
+    struct Elf64_Hdr   *i_header;
+    struct Elf64_Shdr  *i_sections;
+    size_t              i_nr_sections;
+    struct Elf64_Phdr  *i_phdrs;
+    size_t              i_nr_phdrs;
+    char               *i_strtab;        /* NULL terminated */
+    size_t              i_strtab_size;
+
+    void (*i_entry)(uint64_t);
 };
 
 bool elf64_is_header_valid(struct Elf64_Hdr *header);
-struct Elf64_Image *elf64_load(void *image_begin, npa_t target, int *error);
+struct Elf64_Image *elf64_load(void *image_begin, void *image_end, void *target, int *error);
+struct Elf64_Image *ld_load_hvcore(struct MemoryMap *hvmap, int *error);
 
 #endif // LOADER_H
--- sys/loader.c
+++ sys/loader.c
@@ -17,6 +17,11 @@
 #define ARCH_X86     0x3
 #define ARCH_X86_64  0x3E
 
+/* Pointers to the embedded ELF64 core image */
+extern uint32_t __hvcore_start;
+extern uint32_t __hvcore_end;
+extern uint32_t __hvcore_size;
+
 /*
  * Only 64bit, relocatable, SYSV-compatible little-endian
  * valid ELF64 binaries are good for us.
@@ -34,35 +39,142 @@ bool elf64_is_header_valid(struct Elf64_Hdr *header)
         && (header->e_ident[EI_CLASS] == ELFCLASS64)
         && (header->e_ident[EI_DATA]  == ELFDATA2LSB)
         && (header->e_version  == EV_CURRENT)
-        && (header->e_type     == ET_DYN)
-        && (header->e_machine  == ELF_OSABI)
+        && (header->e_type     == ET_EXEC)
         && (header->e_entry    != 0x0);
 }
 
-struct Elf64_Image *elf64_load(void *image_begin, npa_t i_target, int *error)
+static inline struct Elf64_Image *elf64_init_image(struct Elf64_Image *im)
+{
+    im->i_entry    = NULL;
+    im->i_header   = NULL;
+    im->i_phdrs    = NULL;
+    im->i_sections = NULL;
+    im->i_strtab   = NULL;
+    return im;
+}
+
+static struct Elf64_Image *elf64_alloc_image(void)
 {
     struct Elf64_Image *im = NULL;
-    struct Elf64_Hdr *header = NULL;
-    if (!error) {
+    im = mm_expand_heap(sizeof(*im));
+    if (im == NULL) {
         return NULL;
     }
 
-    header = (struct Elf64_Hdr *)image_begin;
-    if (!elf64_is_header_valid(header)) {
+    elf64_init_image(im);
+    return im;
+}
+
+static struct Elf64_Image *elf64_parse(struct Elf64_Image *im, void *image_begin, void *image_end, int *error)
+{
+    struct Elf64_Hdr *ehdr = NULL;
+
+    ehdr = (struct Elf64_Hdr *)image_begin;
+    if (!elf64_is_header_valid(ehdr)) {
         *error = -HV_ENOVALID;
         return NULL;
     }
 
-    im = mm_expand_heap(sizeof(*im));
+    /* The ELF metadata will not be copied into the target, pointers are pointing in the source */
+    im->i_start       = image_begin;
+    im->i_end         = image_end;
+
+    im->i_header      = ehdr;
+    im->i_phdrs       = (struct Elf64_Phdr *)(((uint8_t *)image_begin) + ehdr->e_phoff);
+    im->i_nr_phdrs    = ehdr->e_phnum;
+    im->i_sections    = (struct Elf64_Shdr *)(((uint8_t *)image_begin) + ehdr->e_shoff);
+    im->i_nr_sections = ehdr->e_shnum;
+
+    struct Elf64_Shdr *sh_strtab = im->i_sections + ehdr->e_shstrndx;
+    /* If the .strtab is not valid, why bother with the other stuff */
+    if (sh_strtab->sh_type != SHT_STRTAB || sh_strtab->sh_offset == 0) {
+        *error = -HV_ENOVALID;
+        return NULL;
+    }
+    im->i_strtab      = ((char *)image_begin) + sh_strtab->sh_offset;
+    return im;
+}
+
+struct Elf64_Image *elf64_load(void *image_begin, void *image_end, void *target, int *error)
+{
+    struct Elf64_Image *im = NULL;
+
+    im = elf64_alloc_image();
     if (im == NULL) {
         *error = -HV_ENOMEM;
         return NULL;
     }
-    im->i_header = header;
 
-    struct Elf64_Shdr *sections = mm_expand_heap(sizeof(*sections) * header->e_shnum);
+    elf64_parse(im, image_begin, image_end, error);
+    if (*error != 0) {
+        return NULL;
+    }
 
-    //im->i_entry  = (void(*)(void))header->e_entry;
+    uint8_t *prog_ptr = target; 
+    void *pexec       = NULL;
+    /* The linker script will never load anything to 0x0, hopefully :D */
+    off_t base_off    = 0;
+    struct Elf64_Phdr *phdrs = im->i_phdrs;
+        
+    /* Load the code and data from the program headers */
+    // TODO: Paging protection stuff
+    for (int i = 0; i < im->i_nr_phdrs; i++) {
+        /* Great a loadable segment */
+        if (phdrs[i].p_type == PT_LOAD && phdrs[i].p_paddr != 0 && phdrs[i].p_offset != 0) {
+            uint8_t *prog_src = ((uint8_t *)image_begin) + phdrs[i].p_offset;
+            if (base_off == 0) {
+                /* For the first loaded segment, we need its base 
+                   to compute the others' loading address */
+                base_off = phdrs[i].p_paddr;
+            }
+            /* This is the offset where we going */
+            prog_ptr += phdrs[i].p_paddr - base_off;
 
+            if (phdrs[i].p_flags & (PF_X | PF_R)) {
+                /* Executable, readable program segment, therefore its address is needed 
+                   when entering into it, to execute it later  */
+                pexec = prog_ptr;
+            }
+            /* Load it to the destination, after a cleanup */
+            bzero(prog_ptr, phdrs[i].p_memsz);
+            memcpy(prog_ptr, prog_src, phdrs[i].p_filesz);
+        }
+    }
+
+    if (pexec == NULL) {
+        *error = -HV_ENOVALID;
+        return im;
+    } else {
+        /* Yay entry point */
+        im->i_entry = (void(*)(uint64_t))pexec;
+    }
+
+    return im;
+}
+
+struct Elf64_Image *ld_load_hvcore(struct MemoryMap *hvmap, int *error)
+{
+    struct Elf64_Image *im = NULL;
+    void *im_start = (void *)&__hvcore_start;
+    void *im_end   = (void *)&__hvcore_end;
+    uint32_t laddr = 0; 
+
+    if (error == NULL) {
+        return NULL;
+    } else {
+        *error = 0;
+    }
+
+    if (hvmap == NULL) {
+        *error = -HV_BADARG;
+        return NULL;
+    }
+
+    /* Forcefully truncate size, cannot load anything above 4GB */
+    laddr = (uint32_t)hvmap->mm_start;
+    im = elf64_load(im_start, im_end, (void *)laddr, error);
+    if (*error != 0 || im == NULL) {
+        return NULL;
+    }
     return im;
 }
