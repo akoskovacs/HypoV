e5d859b3296f3f0bb1882d790703f7213d149b9e
Ákos Kovács <akoskovacs0@gmail.com>
2018-01-10 13:31:22 +0100

    Working GDT loading, fixes #32 and closes #7 :birthday:
    	* Remove old code
    	* Set up 32- and 64 bit desciptors :nerd:
    	* Add documentation :ok_hand:
    	* Birthday commit :birthday: :smile:

--- sys/cpu_ll.asm
+++ sys/cpu_ll.asm
@@ -4,17 +4,20 @@
 ; | Low-level CPU stuff                                        |
 ; +------------------------------------------------------------+
 
-global __cpu_gdt64_init
 global __cpu_long_mode_enter
-global __cpu_compat_mode_disable
+global __cpu_call_64
 
 %define CR0_PG_BIT 31
 
-%define DESC_PRESENT    (1 << 15)
-%define DESC_DATA       (1 << 12); bit 11 = 0
-%define DESC_CODE       (1 << 12)|(1 << 11)
-%define LONG_MODE       (1 << 21)
-%define DESC_TSS        (1 << 8)|(1 << 11)
+%define DESC_32BIT          (1 << 22)
+%define DESC_64BIT          (1 << 21)
+%define DESC_PRESENT        (1 << 15)
+%define DESC_CS_READ        (1 << 9)
+%define DESC_DS_WRITE       (1 << 9)
+%define DESC_DATA           (1 << 12); bit 11 = 0
+%define DESC_CODE           (1 << 12)|(1 << 11)
+%define DESC_TSS            (1 << 8)|(1 << 11)
+%define DESC_SEGLIMIT_HIGH  (0xF << 16)
 
 align 4
 section .text
@@ -33,70 +36,84 @@ __cpu_long_mode_enter:
     jmp .halt
 ; End of __cpu_enter_long_mode
 
-; XXX: Must be already in 64 bit mode
-__cpu_gdt64_init:
+
+; Call in to the final 64 bit code, by setting up
+; 32- and 64 bit code and data selector in the 
+; new Global Descriptor Table. The IDT will be cleared.
+
+; XXX: Must be already in 64 bit compatibility mode
+__cpu_call_64:
+    xchg bx, bx
+    ; This function won't return, no need for 
+    ; subroutine prologues, just get the first
+    ; parameter (uint32_t jmp_addr)
+    mov eax, [esp + 4]
+    ; Modify target on-the-fly
+    mov dword [.jmp_addr], eax
+    ; Setup empty LDTR
+    lidt [idt_segment_64]
     ; Setup GDTR
     lgdt [gdt_segment_64]
-    ; Jump with the new CS segment selector
-    jmp 0x8:.reload_segments
-; Set up the other segments too
-.reload_segments:
-    xor eax, eax
+    ; Use the new 64 bit segments
+    mov eax, 0x20
     mov ss, ax
     mov ds, eax
     mov es, eax
     mov fs, eax
     mov gs, eax
-    ret
-; End of __cpu_init_gdt64
-
-; Disable compatilibty mode, enable true 64 bit mode
-; XXX: The 64 bit GDT has to be already present
-__cpu_compat_mode_disable:
-    ; Get the current value
-    mov eax, [gdt_64_seg_cs]
-    ; Enable LONG_MODE bit in the CS segment
-    or eax, LONG_MODE
-    ; Store the segment
-    mov [gdt_64_seg_cs], eax
-    ; Force-reload the old GDT
-    lgdt [gdt_segment_64]
-ret
+    ; Self-modifying machine code to jump directly to the
+    ; desired 64bit code.
+    db 0xEA ; jmp far cs:whatever
+.jmp_addr:
+    dd 0x0  ; Address from EAX
+    dw 0x18 ; 64 bit CS selector
+ret ; __noreturn
+; End of __cpu_call_64
 
 section .data
+%define GDT_NR_ENTRIES 5
 ; Unfortunately, a valid 64bit GDT is still needed.
 ; It is easier to set that up here, in compatilibty mode.
-; The CS.L bit could be set later.
-align 16
+align 8
 gdt_64:
 ; Null entry [0x0]
     dd 0x0 ; limit, base address
     dd 0x0 ; limit, base address and flags
-; CS entry [0x8]
-    ; CPL=0, L=0, CS descriptor, present
-gdt_64_seg_cs:
-    dd DESC_PRESENT | DESC_CODE ; | LONG_MODE
-    dd 0x0 ; no checking on the CS segment
-; SS, DS, ES, FS, GS [0x10]
-    ; CPL=0, L=1, P=1
-    dd DESC_PRESENT | DESC_DATA 
-    dd 0x0 ; checking only for FS, GS (maybe used later)
+; 32bit CS entry [0x8]
+    dw 0xFFFF ; max segment limit
+    dw 0x0    ; base address
+    dd DESC_PRESENT | DESC_CODE | DESC_CS_READ | DESC_32BIT | DESC_SEGLIMIT_HIGH
+; 32bit SS, DS, ES, FS, GS [0x10]
+    dw 0xFFFF ; max segment limit
+    dw 0x0    ; base address
+    dd DESC_PRESENT | DESC_DATA | DESC_32BIT | DESC_DS_WRITE | DESC_SEGLIMIT_HIGH
+; 64bit CS entry [0x18]
+    dw 0xFFFF ; max segment limit
+    dw 0x0    ; base address
+    dd DESC_PRESENT | DESC_CODE | DESC_CS_READ | DESC_64BIT | DESC_SEGLIMIT_HIGH
+; 64bit SS, DS, ES, FS, GS [0x20]
+    dw 0xFFFF ; max segment limit
+    dw 0x0    ; base address
+    dd DESC_PRESENT | DESC_DATA | DESC_DS_WRITE | DESC_64BIT | DESC_SEGLIMIT_HIGH
 ; 64 bit Task State Segment
     ; TSS TODO
 ; 64 bit Interrupt Descriptor
     ; IDT TODO
 
-align 16
+align 8
 gdt_segment_64:
-    dw gdt_segment_64 - gdt_64 - 1
-; The absolute address of the GDT has to be
-; calculated at runtime, because it could be
-; anywhere after relocation
-gdt_addr_64:
-    dq 0x00
+    dw 8 * GDT_NR_ENTRIES
+    dd gdt_64
+    dd 0x0
 
 ; Dummy LDT
-align 16
+align 8
 ldt_segment_64:
-    dw 0x00
-    dq 0x00
+    dw 0x0
+    dq 0x0
+
+; Dummy IDT
+align 8
+idt_segment_64:
+    dw 0x0
+    dq 0x0
